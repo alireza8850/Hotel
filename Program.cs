6a16941bd33d2a09df13455a1a12dc2b7041a223
alireza8850 <alireza.it101@gmail.com>
2025-10-31 09:05:24 +0100

    kompleterade inloggning

--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
-﻿using App;
-
-
-
-Console.WriteLine("5 star Hotel");
-Console.WriteLine("Ange ditt användarnamn: ");
-string? username = Console.ReadLine();
-Console.WriteLine("Ange ditt lösenord: ");
-string? password = Console.ReadLine();
-
-
-
+﻿using System.Collections.Concurrent;
+using App;
 bool running = true;
-
 while (running)
 {
+    Console.Clear();
+    Console.WriteLine("5 star Hotel");
+    Console.WriteLine();
+    Console.WriteLine("Ange ditt användarnamn: ");
+    string? Username = Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("Ange ditt lösenord: ");
+    string? Password = Console.ReadLine();
+    Console.Clear();
+
+    bool ok = Login.TryLogin(Username, Password);
+    if (!ok)
+    {
+        Console.WriteLine("Fel inloggning. Försök igen.");
+        return;
+
+    }
+
+
     // Man ska kunna dessa: Se lista av upptagna rum och lediga rum, boka in, checka ut, markera otillgänligt
     Console.WriteLine("*****Meny*****");
     Console.WriteLine("____________________");
@@ -32,7 +40,13 @@ while (running)
     {
         case "1":
 
-            break;
+        break;
+        case "0":
+            running = false;
+            Console.Clear();
+            Console.WriteLine(":)");
+            
+        break;
         
     }
     
